321ae2721fd78d877fb6f140a5434800c343438f
gv281093@gmail.com <gv281093@gmail.com>
2022-03-27 18:41:02 +0530

    sklearn-code-executed

---
+++ runs.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+
+alpha_s = np.linspace(0.1, 1.0, 5)
+l1_ratios = np.linspace(0.1, 1.0, 5)
+
+for alpha in alpha_s:
+    for l1 in l1_ratios:
+        print(f"logging experiment for alpha:{alpha}, l1: {l1}")
+        os.system(f"python3 demo.py -a {alpha} -l1 {l1}")
